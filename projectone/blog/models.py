841b9e5125163bc4074e2aca9e1508776698bb7f
yushanfans2233 <yushanfans2233@qq.com>
2023-08-15 21:20:27 +0800

    finish 7.2.3

--- projectone/blog/models.py
+++ projectone/blog/models.py
@@ -15,6 +15,21 @@ class Category(models.Model):
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.PROTECT, db_constraint=False)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    @classmethod
+    def get_navs(cls):
+        categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories,
+        }
+
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
@@ -55,6 +70,32 @@ class Post(models.Model):
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.PROTECT, db_constraint=False)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
 
     class Meta:
         verbose_name = verbose_name_plural = '文章'
--- projectone/blog/views.py
+++ projectone/blog/views.py
@@ -7,26 +7,17 @@ def post_list(request, category_id=None, tag_id=None):
     tag = None
     category = None
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
     else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            try:
-                category = Category.objects.get(id=category_id)
-            except Category.DoesNotExist:
-                category = None
-            else:
-                post_list = post_list.filter(category_id=category_id)
+        post_list = Post.latest_posts()
     context = {
         'category': category,
         'tag': tag,
         'post_list': post_list,
     }
+    context.update(Category.get_navs())
     return render(request, 'blog/list.html', context=context)
 
 
@@ -36,5 +27,10 @@ def post_detail(request, post_id):
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         post = None
-    return render(request, 'blogs/detail.html', context={'post': post})
+
+    context = {
+        'post': post,
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context={'post': post})
 
