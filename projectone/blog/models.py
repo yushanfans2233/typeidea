1a9a0dbfef6d9697f0472e44934ff1525c3a0d71
yushanfans2233 <yushanfans2233@qq.com>
2023-09-10 20:48:54 +0800

    feat: rss and sitemap

--- projectone/blog/models.py
+++ projectone/blog/models.py
@@ -1,5 +1,7 @@
+import mistune
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.functional import cached_property
 
 
 class Category(models.Model):
@@ -65,6 +67,7 @@ class Post(models.Model):
     title = models.CharField(max_length=225, verbose_name='标题')
     desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(verbose_name='正文', help_text='正文必须为MarkDown格式')
+    content_html = models.TextField(verbose_name='正文html代码', blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.PROTECT, db_constraint=False)
     tag = models.ManyToManyField(Tag, verbose_name='标签')
@@ -103,6 +106,14 @@ class Post(models.Model):
     def latest_posts(cls):
         queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
         return queryset
+
+    @cached_property
+    def tags(self):
+        return ','.join(self.tag.values_list('name', flat=True))
+
+    def save(self, *args, **kwargs):
+        self.content_html = mistune.markdown(self.content)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = verbose_name_plural = '文章'
--- projectone/projectone/urls.py
+++ projectone/projectone/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.sitemaps import views as sitemap_views
 
 from blog.views import (
     IndexView, CategoryView, TagView,
@@ -25,6 +26,9 @@ from comment.views import CommentView
 from config.views import LinkListView
 from projectone.custom_site import custom_site
 
+from blog.rss import LatestPostFeed
+from blog.sitemap import PostSitemap
+
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
@@ -34,6 +38,9 @@ urlpatterns = [
     url(r'author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
     url(r'^links/$', LinkListView.as_view(), name='links'),
     url(r'^comment/$', CommentView.as_view(), name='comment'),
+
+    url(r'^rss|feed/', LatestPostFeed(), name='rss'),
+    url(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {'posts': PostSitemap}}),
 
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
