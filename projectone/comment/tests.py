e2ac64f16a7ec77d0e6a25555aad0ed29f262b73
imentu <imentu@foxmail.com>
2023-10-26 21:05:36 +0800

    test: post comment

--- projectone/comment/tests.py
+++ projectone/comment/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpResponseRedirect
 
-# Create your tests here.
+
+class TestCommentView(TestCase):
+    def test_post_comment_when_content_length_less_than_10_then_return_validation_error(self):
+        client = Client()
+        form_data = {
+            'nickname': 'jack',
+            'email': 'foo@bar.com',
+            'website': 'https://baidu.com',
+            'content': 'zz',
+            'target': 'https://target.com'
+        }
+
+        response = client.post('/comment/', form_data)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'comment/result.html')
+
+        context = response.context
+        self.assertEquals(context['succeed'], False)
+        self.assertEquals(context['form'].errors['content'][0], '内容长度怎么能这么短呢！！！')
+        self.assertEquals(context['target'], 'https://target.com')
+
+    def test_post_comment_when_valid_then_redirect_to_target(self):
+        client = Client()
+        form_data = {
+            'nickname': 'jack',
+            'email': 'foo@bar.com',
+            'website': 'https://baidu.com',
+            'content': 'zzzzzzzzzzzzz',
+            'target': 'https://target.com'
+        }
+
+        response = client.post('/comment/', form_data)
+
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertEqual(response.url, 'https://target.com')
--- projectone/comment/views.py
+++ projectone/comment/views.py
@@ -7,20 +7,19 @@ class CommentView(TemplateView):
     http_method_name = ['post']
     template_name = 'comment/result.html'
 
-    def post(self, request, *args, **kwargs):
-        comment_form = CommentForm(request.POST)
+    def post(self, request):
+        form = CommentForm(request.POST)
         target = request.POST.get('target')
-        if comment_form.is_valid():
-            instance = comment_form.save(commit=False)
-            instance.target = target
-            instance.save()
-            succeed = True
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.target = target
+            comment.save()
             return redirect(target)
-        else:
-            succeed = False
+
         context = {
-            'succeed': succeed,
-            'form': comment_form,
+            'succeed': False,
+            'form': form,
             'target': target,
         }
         return self.render_to_response(context)
